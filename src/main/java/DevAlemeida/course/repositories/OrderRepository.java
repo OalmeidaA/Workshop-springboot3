f330be00d47c9fb1843a0b76e0d4cb2223ab353a
Henrique Alves <155191957+HenrykeAlmeida@users.noreply.github.com>
2025-03-21 10:18:37 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/DevAlemeida/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package DevAlemeida.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import DevAlemeida.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
